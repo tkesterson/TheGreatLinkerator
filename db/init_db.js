22dbe647f1b8c1b4f2266f410405156b9dca29c8
rashadharrison <harrisonmartin26@gmail.com>
2021-06-10 19:37:40 -0400

    added tables to db

--- db/init_db.js
+++ db/init_db.js
@@ -6,11 +6,52 @@ const {
 
 async function buildTables() {
   try {
+
     client.connect();
+    console.log()
+    await client.query(`
+    DROP TABLE IF EXISTS link_tags;
+    DROP TABLE IF EXISTS tags;
+    DROP TABLE IF EXISTS links;`
+    )
+    
 
     // drop tables in correct order
 
     // build tables in correct order
+
+    await client.query(`
+    CREATE TABLE links (
+
+      id SERIAL PRIMARY KEY,
+      name varchar(255) UNIQUE NOT NULL,
+      count INTEGER,
+      comments varchar(255) NOT NULL,
+      date DATE NOT NULL,
+      url varchar(255) UNIQUE NOT NULL,
+    
+      
+    );
+
+    CREATE TABLE tags (
+      id SERIAL PRIMARY KEY,
+      name varchar(255) UNIQUE NOT NULL,
+
+
+
+    );
+
+    CREATE TABLE links_tags (
+    id SERIAL PRIMARY KEY,
+    "linkTagsId" REFERENCES links(id),
+    "tagId" REFERENCES tags(id)
+
+
+    );
+    
+    
+    
+    `)
 
   } catch (error) {
     throw error;
